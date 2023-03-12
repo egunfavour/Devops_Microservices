3049e4a480428e69f5bec17575d03fdc40b67556
egunfavour <egunfavour@gmail.com>
2023-03-12 02:04:42 +0000

    task 2

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=favour555/udacity-devops-project .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+#docker run -t udacity-devops-project bash
+docker run -p 8000:80 favour555/udacity-devops-project
