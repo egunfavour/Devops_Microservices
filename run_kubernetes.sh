85ff952c9322fb280b2f89beaec541840f5c77bd
egunfavour <egunfavour@gmail.com>
2023-03-13 10:58:22 +0000

    circleci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="favour555/udacity-devops-project"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-devops-project\
+	--image=$dockerpath\
+	--port=80 --labels app=udacity-devops-project
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacity-devops-project 8000:80
+
 
